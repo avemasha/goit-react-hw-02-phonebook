8993ea7b2e97e2c171afffdc5f61e7114db6576b
Maria Novosad <100295231+avemasha@users.noreply.github.com>
2023-05-03 14:13:55 +0200

    filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,19 @@ import Header from "./Header/Header";
 import React, { Component} from "react";
 import PhonebookForm from "./PhonebookForm/PhonebookForm";
 import ContactsList from "./Contacts/ContactsList";
+import FilterForm from './Filter/Filter';
 
 
   class App extends Component {
 
     state = {
-      contacts: [],
+      contacts: [
+        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      ],
+      filter:'',
       
     }
 
@@ -62,6 +69,19 @@ import ContactsList from "./Contacts/ContactsList";
       this.setState( {name: e.currentTarget.value})
     }
   
+    setFilterToState = filterData => {
+      this.setState({ ...this.state, filter: `${filterData}` });
+
+    
+    };
+
+    
+  filterArr = fArr => {
+    let newArr = fArr.filter(cur =>
+      cur.name.toUpperCase().includes(this.state.filter),
+    );
+    return newArr;
+  };
 
     // addContact = name => {
     //   console.log(name)
@@ -88,7 +108,10 @@ import ContactsList from "./Contacts/ContactsList";
         <Header header="Phonebook"></Header>
         <PhonebookForm onSubmit = {this.formSubmitHandler}     ></PhonebookForm>
         <Header header="Contacts"></Header>
-        <ContactsList contacts={this.state.contacts}></ContactsList>
+        <FilterForm setFilterToState={this.setFilterToState}></FilterForm>
+        <ContactsList contacts={this.filterArr(this.state.contacts)}>
+        
+        </ContactsList>
     </div>
      ) 
     };
---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+import './Filter.css'
+
+
+class FilterForm extends Component {
+    setFilterValue = event => {
+        let value = event.currentTarget.value.toUpperCase();
+        this.props.setFilterToState(value);
+    }
+        render() {
+            return (
+              <div>
+                <h4>Find contacts by name</h4>
+                <input placeholder="Enter contact name" onChange={this.setFilterValue}></input>
+              </div>
+            );
+          }
+    }
+
+
+    export default FilterForm
